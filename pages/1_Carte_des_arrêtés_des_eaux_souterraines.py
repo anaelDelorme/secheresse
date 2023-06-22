dacf71e3f16500a34cd9cc6303288e8b4be5c637
Anaël Delorme <anael.delorme@agriculture.gouv.fr>
2023-06-22 13:21:09 +0200

    encore et encore

--- pages/1_Carte_des_arrêtés_des_eaux_souterraines.py
+++ pages/1_Carte_des_arrêtés_des_eaux_souterraines.py
@@ -30,15 +30,11 @@ st.markdown(
     unsafe_allow_html=True
 )
 with st.spinner('Chargement en cours...'):
-    gdf_sup = recup_data_arrete_du_jour()
-    latitude = 46.1
-    longitude = 2.2
-    m = folium.Map(location=[latitude, longitude], zoom_start=5)
-
-    niveaux = ['Vigilance', 'Alerte', 'Alerte renforcée', 'Crise']
-    couleurs = ['#FAED93', '#FAC939', '#FA78C5', '#FA2048']
-    couleur_map = dict(zip(niveaux, couleurs))
-
+    
+    data_geo_simplify = recup_zones_actives()
+    arretes = recup_data_arrete_du_jour()
+    arretes_publie = arretes[arretes['statut_arrete'] == "Publié"]
+    geo_merge = data_geo_simplify.merge(arretes_publie, on = 'id_zone')
     colonnes_selectionnees = ['id_zone',
                                 'code_zone',	
                                 'type_zone',
@@ -52,20 +48,33 @@ with st.spinner('Chargement en cours...'):
                                 'date_signature',	
                                 'debut_validite_arrete',	
                                 'fin_validite_arrete']
-    
+
+    gdf_selection = geo_merge.loc[:, colonnes_selectionnees]
+    gdf_non_vide = gdf_selection[~gdf_selection['geometry'].is_empty & gdf_selection['geometry'].notna()].dropna(subset=['geometry'])
+    gdf_sup = gdf_non_vide[gdf_non_vide['type_zone'] == "SUP"]
+
+    latitude = 46.1
+    longitude = 2.2
+    m = folium.Map(location=[latitude, longitude], zoom_start=5)
+
+    niveaux = ['Vigilance', 'Alerte', 'Alerte renforcée', 'Crise']
+    couleurs = ['#FAED93', '#FAC939', '#FA78C5', '#FA2048']
+    couleur_map = dict(zip(niveaux, couleurs))
+
     colonnes_tooltip = colonnes_selectionnees.copy()
     colonnes_tooltip.remove('geometry')
+
 
     colonnes_tooltip_alias = []
     for colonne in colonnes_tooltip:
         colonne_alias = colonne.replace("_", " ").capitalize()
         colonnes_tooltip_alias.append(colonne_alias)
 
-        folium.GeoJson(gdf_sup, style_function=style_function, tooltip=folium.features.GeoJsonTooltip(
+    folium.GeoJson(gdf_sup, style_function=style_function, tooltip=folium.features.GeoJsonTooltip(
                 fields=colonnes_tooltip,
                 aliases=colonnes_tooltip_alias,
                 sticky=True,
                 opacity=0.9,
                 direction='right',
-        )).add_to(m)
-    folium_static(m)    
+            )).add_to(m)
+    folium_static(m)
